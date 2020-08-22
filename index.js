2ad345ac5349e978404fe500cea7c9d0cc602a28
ChiragJain123 <reenajain.rj10051976@gmail.com>
2020-08-22 23:34:55 +0530

    Sound Problem Solved Finally : retry-3rd

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3'); 
-audio.loop=true;
-audio.play();
+$(document).ready(function(){
+    alert('To Play Sound Click on The car. Caution: Click the car ONLY FOR 1 TIME!!!');
+  });
+$(".btn").on("click", function () {
+    var audio = document.createElement('audio');
+    audio.setAttribute('src', 'sound.mp3');
+    audio.loop = true;
+    audio.play();
+});
